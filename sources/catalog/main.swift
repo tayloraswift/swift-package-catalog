4a9ffbb123b9e550ced5d64d495bb8cd28886dd9
kelvin13 <kelvin13ma@gmail.com>
2022-04-02 18:06:45 -0500

    add support for including dependencies in catalog, fixes #1

--- sources/catalog/main.swift
+++ sources/catalog/main.swift
@@ -14,44 +14,90 @@ enum DocumentationExtractionError:Error, CustomStringConvertible
     }
 }
 
+extension Target 
+{
+    func gather(dependencies:inout [String: SwiftSourceModuleTarget])
+    {
+        if let target:SwiftSourceModuleTarget = self as? SwiftSourceModuleTarget 
+        {
+            dependencies[target.id] = target
+        }
+        for dependency:TargetDependency in self.dependencies 
+        {
+            switch dependency 
+            {
+            case .target(let target):
+                target.gather(dependencies: &dependencies)
+            case .product(let product):
+                for target:any Target in product.targets 
+                {
+                    target.gather(dependencies: &dependencies)
+                }
+            }
+        }
+    }
+}
+extension Package
+{
+    func gather(nationalities:inout [String: Package])
+    {
+        for dependency:PackageDependency in self.dependencies 
+        {
+            dependency.package.gather(nationalities: &nationalities)
+        }
+        for target:any Target in self.targets
+        {
+            nationalities[target.id] = self
+        }
+    }
+}
 @main 
 struct Main:CommandPlugin
 {
     func performCommand(context:PluginContext, arguments:[String]) throws 
     {
-        var invitations:Set<String> = .init(arguments)
+        // determine which targets belong to which packages 
+        var nationalities:[String: Package] = [:]
+        context.package.gather(nationalities: &nationalities)
+        
+        var targets:[String: SwiftSourceModuleTarget] = [:]
+        for target:any Target in context.package.targets
+        {
+            target.gather(dependencies: &targets)
+        }
+        
+        var invited:Set<String> = .init(arguments)
+        let filtered:[SwiftSourceModuleTarget] = targets.values.filter
+        {
+            arguments.isEmpty || invited.contains($0.name)
+        }
         
         let options:PackageManager.SymbolGraphOptions = .init(
             minimumAccessLevel: .public,
             includeSynthesized: true,
             includeSPI: true)
         
-        var modules:[String] = [], 
-            include:[String] = []
-        for target:any Target in context.package.targets
+        var packages:[String: [String: [String]]] = [:]
+        for target:SwiftSourceModuleTarget in filtered
         {
-            guard let target:SwiftSourceModuleTarget = target as? SwiftSourceModuleTarget
+            guard let package:Package = nationalities[target.id] 
             else 
             {
-                continue 
-            }
-            if case nil = invitations.remove(target.name), !arguments.isEmpty
-            {
-                continue 
+                fatalError("unreachable")
             }
             for file:File in target.sourceFiles
             {
                 if  case .unknown = file.type, 
                     case "docc"?  = file.path.extension?.lowercased()
                 {
-                    include.append(file.path.string)
+                    packages[package.id, default: [:]][target.name, default: []].append(file.path.string)
                 }
             }
             let graphs:PackageManager.SymbolGraphResult = try self.packageManager.getSymbolGraph(for: target, options: options)
-            include.append(graphs.directoryPath.string)
-            modules.append(target.name)
+            packages[package.id, default: [:]][target.name, default: []].append(graphs.directoryPath.string)
+            invited.remove(target.name)
         }
-        for missing:String in invitations
+        for missing:String in invited
         {
             throw DocumentationExtractionError.missingTarget(missing)
         }
@@ -60,12 +106,17 @@ struct Main:CommandPlugin
         let index:String = 
         """
         [
+        \(packages.sorted { $0.key < $1.key }.map 
+        {
+        """
             {
                 "format": "entrapta",
-                "package": "\(context.package.id)", 
-                "modules": [\(modules.map { "\"\($0)\"" }.joined(separator: ", "))],
-                "include": [\(include.map { "\"\($0)\"" }.joined(separator: ", "))]
+                "package": "\($0.key)", 
+                "modules": [\($0.value.keys.sorted().map { "\"\($0)\"" }.joined(separator: ", "))],
+                "include": [\($0.value.values.joined().sorted().map { "\"\($0)\"" }.joined(separator: ", "))]
             }
+        """
+        }.joined(separator: ",\n"))
         ]
         """
         print(index)
